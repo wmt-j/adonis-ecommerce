1bf6e48bc3d6d4fe095223d0bd9189344edc320d
Jeenit Sharma <jeenits@webmobtech.com>
2022-09-12 14:33:29 +0530

    Bug fix

--- app/utils/paginate.ts
+++ app/utils/paginate.ts
@@ -1,5 +1,6 @@
 export default async function (query, page: number, limit: number) {
-    const results = await query.limit(limit).skip((page - 1) * limit)
+    const docs = await query.find()
+    const results = await query.limit(limit).skip((page - 1) * limit).clone()
 
-    return { pageInfo: { currentPage: page, totalPages: Math.ceil(page / limit) }, results }
+    return { pageInfo: { currentPage: page, totalPages: Math.ceil(docs.length / limit) }, results }
 }
