220e8ddf6a0bf8a260fe07c27f8d3444ab3763b8
Jeenit Sharma <jeenits@webmobtech.com>
2022-09-12 14:11:57 +0530

    paginate change

--- app/utils/paginate.ts
+++ app/utils/paginate.ts
@@ -1,4 +1,5 @@
 export default async function (query, page: number, limit: number) {
     const results = await query.limit(limit).skip((page - 1) * limit)
-    return { page, limit, count: results.length, results }
+
+    return { pageInfo: { currentPage: page, totalPages: Math.ceil(page / limit) }, results }
 }
